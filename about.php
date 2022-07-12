306b8df66aaf7a5220589b1dc27cb29d7717bd00
Emad-ahmed <amadahmed1234678@gmail.com>
2022-07-11 21:09:15 -0700

    chnges

--- about.php
+++ about.php
@@ -26,7 +26,7 @@ session_start();
 <body>
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.html"><img src="img/logo.png" alt="" class="logopng"></a>
+            <a class="navbar-brand" href="index.php"><img src="img/logo.png" alt="" class="logopng"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
